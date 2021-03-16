1a127957d5a70446c97f50289750a95f24e14e6c
Javier Diaz <coderdiaz@gmail.com>
2021-03-16 14:29:31 -0600

    chore: clean unnecessary code

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,15 +3,13 @@ import DefaultLayout from "@/layouts/Default";
 const DashboardPage = () => {
   return <>
     <section>
-      <div>
-        <div className="min-h-screen flex flex-col justify-center">
-          <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <p className="text-center text-4xl">
-              Bienvenido <span className="font-bold">Javier</span>
-            </p>
-            <div className="text-center">
-              <img src="/assets/svg/placeholder.svg" />
-            </div>
+      <div className="min-h-screen flex flex-col justify-center">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <p className="text-center text-4xl">
+            Bienvenido <span className="font-bold">Javier</span>
+          </p>
+          <div className="text-center">
+            <img src="/assets/svg/placeholder.svg" />
           </div>
         </div>
       </div>
