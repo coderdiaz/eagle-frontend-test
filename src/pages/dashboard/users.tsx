162e186d7ba67b2c545346b9f66306ea89738f68
Javier Diaz <coderdiaz@gmail.com>
2021-03-16 14:35:15 -0600

    fix: fixed columns name

--- src/pages/dashboard/users.tsx
+++ src/pages/dashboard/users.tsx
@@ -29,41 +29,41 @@ const UsersPage = () => {
           <table className="min-w-full">
             <thead>
               <tr className="border-t border-gray-200">
-                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-medium text-blueGray-500 uppercase tracking-wider">
-                  <span className="lg:pl-2">Name</span>
+                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-semibold text-blueGray-500 uppercase tracking-wider">
+                  <span className="lg:pl-2">Nombre</span>
                 </th>
-                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-medium text-blueGray-500 uppercase tracking-wider">
-                  Username
+                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-semibold text-blueGray-500 uppercase tracking-wider">
+                  Nombre de usuario
                 </th>
-                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-medium text-blueGray-500 uppercase tracking-wider">
-                  Phone
+                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-semibold text-blueGray-500 uppercase tracking-wider">
+                  Teléfono
                 </th>
-                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Email
+                <th className="px-6 py-3 border-b border-blueGray-200 bg-blueGray-50 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                  Correo Electrónico
                 </th>
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-100">
               { users.map((user, index) => <tr key={index}>
-                <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
+                <td className="px-6 py-3 whitespace-nowrap text-sm font-semibold text-gray-900">
                   <div className="flex items-center space-x-3 lg:pl-2">
                     <div className="flex-shrink-0 w-2.5 h-2.5 rounded-full bg-pink-600" aria-hidden="true" />
                     <span>{user?.name}</span>
                   </div>
                 </td>
                 <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                  <div className="flex items-center space-x-3 lg:pl-2">
+                  <div className="flex items-center space-x-3">
                     <span>{user?.username}</span>
                   </div>
                 </td>
                 <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                  <div className="flex items-center space-x-3 lg:pl-2">
+                  <div className="flex items-center space-x-3">
                     <span>{user?.phone}</span>
                   </div>
                 </td>
                 <td className="px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                  <div className="flex items-center space-x-3 lg:pl-2">
-                    <span>@{user?.email}</span>
+                  <div className="flex items-center space-x-3">
+                    <span>{user?.email}</span>
                   </div>
                 </td>
               </tr>)}
