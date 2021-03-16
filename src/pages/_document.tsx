5621ab3f4ad841e6ccb02bbb5fb5d09277b413c0
Javier Diaz <coderdiaz@gmail.com>
2021-03-15 20:08:18 -0600

    feat: added custom document with default global styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body className="leading-normal antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
