051ee8221b311b4d192b8509e75ef2533f7d2c96
Javier Diaz <coderdiaz@gmail.com>
2021-03-16 13:21:16 -0600

    feat: changed colors and font family in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 const colors = require('tailwindcss/colors');
+const defaultConfig = require('tailwindcss/defaultConfig');
 
 module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ["Montserrat", ...defaultConfig.theme.fontFamily.sans],
+    },
     extend: {
       colors: {
         green: colors.teal,
+        gray: colors.gray,
+        coolGray: colors.coolGray,
+        blueGray: colors.blueGray,
       }
     },
   },
