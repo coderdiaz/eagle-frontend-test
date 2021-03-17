1fb43f5df4790c9520708abfbef32f94af43393f
Javier Diaz <coderdiaz@gmail.com>
2021-03-17 01:02:26 -0600

    feat: show in notifications number of tasks undone

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,5 +1,6 @@
-import { ComponentType } from "react";
+import { ComponentType, useContext } from "react";
 import Sidebar from "@/components/Sidebar";
+import { Context, ContextValues } from "@/components/TasksContext";
 
 type ILayoutOptions = {
   title: string;
@@ -7,6 +8,8 @@ type ILayoutOptions = {
 
 const DefaultLayout = (Component: ComponentType, options: ILayoutOptions) => {
   return () => {
+    const { tasks } = useContext(Context) as ContextValues;
+
     return <>
       <div className="h-screen flex overflow-hidden">
         <Sidebar />
@@ -27,7 +30,7 @@ const DefaultLayout = (Component: ComponentType, options: ILayoutOptions) => {
                     <svg className="w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                     </svg>
-                    <span className="inline-flex rounded-full bg-blue-600 text-white w-5 text-xs items-center justify-center absolute top-0 right-0 border-2 border-blueGray-200">0</span>
+                    <span className="inline-flex rounded-full bg-blue-600 text-white w-5 text-xs items-center justify-center absolute top-0 right-0 border-2 border-blueGray-200">{tasks.filter(task => !task.done).length}</span>
                   </a>
                 </div>
                 <div className="flex space-x-4 items-center">
