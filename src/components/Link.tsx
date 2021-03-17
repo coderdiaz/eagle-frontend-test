e6014c0a5e3a2f4cd74364b74f0403dac073c3d6
Javier Diaz <coderdiaz@gmail.com>
2021-03-17 01:28:35 -0600

    fix: removing warning to anonymous named components

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useRouter } from 'next/router'
 
-export default ({ href, children }) => {
+const Link = ({ href, children }) => {
   const router = useRouter();
 
   let className = children.props.className || '';
@@ -10,5 +10,7 @@ export default ({ href, children }) => {
     className = `${className} border-l-2 border-indigo-100 bg-gray-400 text-indigo-100 bg-opacity-10`;
   }
 
-  return <Link href={href}>{React.cloneElement(children, { className })}</Link>;
+  return <NextLink href={href}>{React.cloneElement(children, { className })}</NextLink>;
 }
+
+export default Link;
