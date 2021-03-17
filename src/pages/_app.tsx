eb8e5047da5be848252dd8478cc1eb71a4fcbf33
Javier Diaz <coderdiaz@gmail.com>
2021-03-17 01:01:49 -0600

    feat: loaded context in app wapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
+import TasksContext from "@/components/TasksContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <TasksContext>
+    <Component {...pageProps} />;
+  </TasksContext>
 };
 
 export default MyApp;
